c381bcb27bdd6a65f832fe471cec201fa1eda109
Helvlaska <ccastan@live.fr>
2025-07-28 16:38:18 +0200

    ✨ (2-floor): Ajout du module avec la fonction pour retourner la partie entière d'un flottant

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module 2-floor
+This module defines a function that returns the floor of a float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a floating-point number.
+
+    Args:
+        n (float): The number to floor.
+
+    Returns:
+        int: The largest integer less than or equal to n.
+    """
+    return int(n)
