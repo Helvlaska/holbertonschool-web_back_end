d641e89711a6f43b96c006ba3bda5051b0302b77
Helvlaska <ccastan@live.fr>
2025-07-30 09:39:28 +0200

    ✨ (3-to_str): Ajout du module pour convertir un flottant en chaîne de caractères

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module 3-to_str
+This module defines a function that converts a float to a string.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to its string representation.
+
+    Args:
+        n (float): The float number to convert.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
