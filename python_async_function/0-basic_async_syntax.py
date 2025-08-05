94bb796c2c4fba37c2263748297914eea99327a1
Helvlaska <ccastan@live.fr>
2025-08-05 20:14:02 +0200

    🔨 (0-basic_async_syntax.py): Correction de l'indentation dans la documentation de la fonction wait_random

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,8 +3,10 @@
 import asyncio
 import random
 
-async def wait_random(max_delay: int =10) -> float:
-    """Wait for a random delay between 0 and max_delay (included) and return it.
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Wait for a random delay between 0 and max_delay (included) and
+    return it.
 
     Args:
         max_delay (int): The maximum delay in seconds. Defaults to 10.
