5c4ba34442e6222c541bda8d0b47aae9fd787249
Helvlaska <ccastan@live.fr>
2025-08-05 20:11:54 +0200

    🔨 (0-basic_async_syntax.py): Amélioration de la documentation de la fonction wait_random pour inclure des annotations de type et des descriptions d'arguments

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,7 +3,15 @@
 import asyncio
 import random
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int =10) -> float:
+    """Wait for a random delay between 0 and max_delay (included) and return it.
+
+    Args:
+        max_delay (int): The maximum delay in seconds. Defaults to 10.
+
+    Returns:
+        float: The actual delay waited.
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
