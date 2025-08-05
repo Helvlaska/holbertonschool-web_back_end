3b60ab6c05ec7be96fe1f9bed50e1ecea3ede0dc
Helvlaska <ccastan@live.fr>
2025-08-05 23:43:30 +0200

    🔨 (0-async_generator.py): Correction du type d'annotation de retour pour la fonction async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yield a random float between 0 and 10 every second, 10 times."""
     for _ in range(10):
         await asyncio.sleep(1)
