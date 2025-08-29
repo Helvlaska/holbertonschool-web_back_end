bd6da6d8d2decb981007ab4c12b00d8450dbe761
Helvlaska <ccastan@live.fr>
2025-08-29 17:54:11 +0200

    :hammer: (8-clean_set.js): Refactorisation de la fonction cleanSet pour améliorer la lisibilité et la performance

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,22 @@
 export default function cleanSet(set, startString) {
-  // Vérifie si startString est une string vide
-  if (startString === '') return '';
+  // Vérifie si startString est vide ou n'est pas de type string
+  if (startString === "" || typeof startString !== "string") {
+    return "";
+  }
 
-  const arrayFromSet = Array.from(set);     // Transforme le set en array
+  // Création d'un array pour stocker les nouvelles values
+  const cleanValue = [];
 
-  // Garder que les values qui commencent par startString
-  const filtered = arrayFromSet.filter(value => value.startsWith(startString));
-
-  // Retire startString des values
-  const cleaned = filtered.map(value => value.slice(startString.length));
-
-  // Formatage de la string
-  const result = cleaned.join('-');
-
-  return result;
+  // Boucle pour parcourir les valeurs du set
+  for (const value of set) {
+    // Vérifie si value commence par startString et si de type string
+    if (value.startsWith(startString) && typeof value === "string") {
+      // Retire startString de value
+      const cleanStartValue = value.slice(startString.length);
+      // Envoie des nouvelles values dans le tableau
+      cleanValue.push(cleanStartValue);
+    }
+  }
+  // Retourne la nouvelle string fromatée et réassemblée
+  return cleanValue.join("-");
 }
