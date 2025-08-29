2a5a5aafcdf5929dc99bc871eb3501ad45fc0be6
Helvlaska <ccastan@live.fr>
2025-08-29 20:15:54 +0200

    ✨ (1-promise.js): Ajout de la fonction getFullResponseFromAPI pour créer et renvoyer une promesse

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+  // Créer et renvoie une nouvelle promesse
+  return new Promise((resolve, reject) => {
+
+    // Vérifie si le paramètre de fonction vaut true ou false
+    if (success === true) {
+      resolve({status: 200, body:'Success'});
+    } else {
+      reject("The fake API is not working currently");
+    }
+  });
+}
