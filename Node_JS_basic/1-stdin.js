7aadd4f709a29e06b786b687d209e5a747242738
Helvlaska <ccastan@live.fr>
2025-09-03 11:10:01 +0200

    :hammer: (1-stdin.js): Correction des guillemets pour l'affichage des messages en console

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,14 @@
 // Affiche un message en console
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Permet l'entré de données par le user
-process.stdin.on("data", (data) => {
+process.stdin.on('data', (data) => {
   // Affiche le message en console avec la nouvelle donnée
   process.stdout.write(`Your name is: ${data}`);
 });
 
 // Fermeture de l'entrée de donnée
-process.stdin.on("end", () => {
+process.stdin.on('end', () => {
   // Affiche du message de fermeture
-  process.stdout.write("This important software is now closing\n");
+  process.stdout.write('This important software is now closing\n');
 });
