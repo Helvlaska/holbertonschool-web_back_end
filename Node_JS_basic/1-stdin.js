bc9192fba82d0b7b8d484ea2aa2840f6239eaf7d
Helvlaska <ccastan@live.fr>
2025-09-03 10:44:42 +0200

    :hammer: (1-stdin.js): Suppression de la conversion et du nettoyage de l'entrée utilisateur pour afficher directement la donnée

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,10 +3,8 @@ process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 // Permet l'entré de données par le user
 process.stdin.on("data", (data) => {
-  // Convertie l'entrée en string et la nettoie de ses espaces
-  const name = data.toString().trim();
   // Affiche le message en console avec la nouvelle donnée
-  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 // Fermeture de l'entrée de donnée
