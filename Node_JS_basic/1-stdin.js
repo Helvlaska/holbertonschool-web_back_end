1855707d5429908c8f62e204501ddbbf64187706
Helvlaska <ccastan@live.fr>
2025-09-03 10:35:24 +0200

    :hammer: (1-stdin.js): Réorganisation de la gestion de l'entrée de données pour fermer correctement le processus

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,7 +6,9 @@ process.stdin.on("data", (data) => {
   // Convertie la donnée en string et la nettoie des espaces
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  console.log("This important software is now closing");
-  // Ferme l'entrée de données
-  process.exit();
 })
+
+// fermeture de l'entrée de données
+process.stdin.on("end", () => {
+  console.log("This important software is now closing");
+});
