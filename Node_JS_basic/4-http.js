ae72f8d3fee0972be3654d117ee8cc1bae10f2a8
Helvlaska <ccastan@live.fr>
2025-09-05 17:07:31 +0200

    ✨ (4-http.js): Ajout d'un serveur HTTP qui répond avec un message simple

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,17 @@
+// module pour créer un serveur
+const http = require('http');
+
+// Créer le serveur
+const app = http.createServer((req, res) => {
+  // La réponse est un texte brut avec un code OK 200
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // Envoie un message en console et termine la réponse
+  res.end('Hello Holberton School!');
+});
+
+// Demande au serveur de rester à l'écoute
+// eslint-disable-next-line jest/require-hook
+app.listen(1245);
+
+// Permet l'exportation de la fonction dans d'autres modules
+module.exports = app;
